d1ce6012b0628823689d93a8a9a90558f0791035
Gerardo Gamas <kukulcan_gamas@hotmail.com>
2021-02-06 19:37:10 -0500

    proyecto Hipsterco

---
+++ ModernUI_WPF/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ModernUI_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+    }
+}
